fe622844b58ec2bf3ea47f7eb7cc1d3bd55d8c3f
jFuentesj <jfuentesj@ia.com.mx>
2018-03-02 13:04:37 -0600

    Cambios acerca de

--- app/src/main/java/com/example/amartineza/pruebagitequipo/AcercaDeActivity.java
+++ app/src/main/java/com/example/amartineza/pruebagitequipo/AcercaDeActivity.java
@@ -2,12 +2,23 @@ package com.example.amartineza.pruebagitequipo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class AcercaDeActivity extends AppCompatActivity {
+
+    TextView tv_DatosNombre;
+    TextView tv_DatosEscuela;
+    TextView tv_DatosAcerca;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_acerca_de);
+
+        tv_DatosNombre = (TextView) findViewById(R.id.tv_nombre);
+        tv_DatosEscuela = (TextView) findViewById(R.id.tv_escuela);
+        tv_DatosAcerca = (TextView) findViewById(R.id.tv_acecade);
+
     }
 }
