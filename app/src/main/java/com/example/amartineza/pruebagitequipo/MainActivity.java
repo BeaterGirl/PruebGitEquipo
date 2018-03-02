ca0dd39ae5f139f6862e24c3c4245d1f9a5e1865
amartineza <amartineza@ia.com.mx>
2018-03-02 12:57:36 -0600

    Pantalla Principal

--- app/src/main/java/com/example/amartineza/pruebagitequipo/MainActivity.java
+++ app/src/main/java/com/example/amartineza/pruebagitequipo/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.amartineza.pruebagitequipo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +12,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button btnGoAcercaDe = (Button)findViewById(R.id.btn_go_Acercade);
+        Button btnGoForm = (Button)findViewById(R.id.btn_go_Formulario);
+
+        btnGoAcercaDe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        btnGoForm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
 
 
     }
